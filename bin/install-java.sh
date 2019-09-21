dc356287c7076b9697e4560b7be66838f2edcdac
Ólafur Páll Geirsson <olafurpg@gmail.com>
2019-09-21 12:49:49 +0100

    Add Java installation script

---
+++ bin/install-java.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ex
+export ADOPTOPENJDK=${JAVA_VERSION:-8}
+curl -sL https://get.sdkman.io | bash
+echo sdkman_auto_answer=true > $HOME/.sdkman/etc/config
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk install java $(sdk list java | grep -o "$ADOPTOPENJDK\.[0-9\.]*hs-adpt" | head -1)
+unset JAVA_HOME
+java -Xmx32m -version
+git fetch --tags
